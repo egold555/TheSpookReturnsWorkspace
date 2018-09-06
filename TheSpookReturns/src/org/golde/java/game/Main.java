72538c18b6da2b725a9312cd048fcd6a11f61e03
Eric Golde <eric@golde.org>
2018-09-05 19:15:16 -0700

    Fixed broken code from merge

--- TheSpookReturns/src/org/golde/java/game/Main.java
+++ TheSpookReturns/src/org/golde/java/game/Main.java
@@ -13,6 +13,7 @@ import org.golde.java.game.gui.base.GuiText;
 import org.golde.java.game.gui.mainMenu.GuiMainMenu;
 import org.golde.java.game.gui.mainMenu.GuiOptions;
 import org.golde.java.game.gui.player.GuiDebug;
+import org.golde.java.game.gui.player.GuiFade;
 import org.golde.java.game.helpers.BetterKeyboard;
 import org.golde.java.game.helpers.BlankLogger;
 import org.golde.java.game.multiplayer.MPlayer;
@@ -157,7 +158,7 @@ public class Main {
 	//End
 
 	static boolean wireframe = false;
-	
+
 	public static void main(String[] args) {
 		//**********Basic Setup***************
 		DisplayManager.createDisplay();
@@ -245,7 +246,7 @@ public class Main {
 		lights.add(new Light(new Vector3f(2000, 2000, 0), new Vector3f(0.2f, 0.2f, 0.2f))); //Sun
 		entities.add(new EntityLamp(loader, 100, 10, terrain1, 1)); //.setAttenuation(0.5f, 0.003f, 0.0005f).setSpotLight(new Vector3f(-1, -0.1F, -0.15F), 10, 30)
 		entities.add(new EntityLamp(loader, -100, 10, terrain1, 1).setColor(new Vector3f(2, 0, 0)));
-		
+
 		entities.add(new EntityFirepit(loader, -60, 60, terrain1, 1));
 		entities.add(new EntityOilDrum(loader, 0, 80, terrain1, 10));
 
@@ -257,148 +258,155 @@ public class Main {
 
 		//Sort all registered Guis by Z index
 		Collections.sort(guis);
-		
+
 		//entities.add(new EntityTV(loader, 50, -50, terrain1, 0.8f)); // 0.8f
 		entities.add(new EntityTV(loader, 50, -50, terrain1, 0.8f, "Wrecked VHS.mp4")); // 0.8f Wrecked VHS.mp4
-		
+
 		//Water
 		List<WaterTile> water = new ArrayList<WaterTile>();
 		WaterTile waterTile = new WaterTile(90, -117, 0);
 		water.add(waterTile);
-		
-		
-//		GuiStaticTexture waterTestTextureReflection = new GuiStaticTexture(fbos.getReflectionTexture(), new Vector2f(0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
-//		GuiStaticTexture waterTestTextureRefraction = new GuiStaticTexture(fbos.getRefractionTexture(), new Vector2f(-0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
-//		
-//		player.getGuiOverlay().addGuiTexture(waterTestTextureReflection);
-//		player.getGuiOverlay().addGuiTexture(waterTestTextureRefraction);
-		
+
+
+		//		GuiStaticTexture waterTestTextureReflection = new GuiStaticTexture(fbos.getReflectionTexture(), new Vector2f(0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
+		//		GuiStaticTexture waterTestTextureRefraction = new GuiStaticTexture(fbos.getRefractionTexture(), new Vector2f(-0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
+		//		
+		//		player.getGuiOverlay().addGuiTexture(waterTestTextureReflection);
+		//		player.getGuiOverlay().addGuiTexture(waterTestTextureRefraction);
+
 		//player.setHasGravity(false);
-		
+
 		//Final
 		DisplayManager.aboutToStartGameLoop();
 
 		//Main Loop
 		while(!Display.isCloseRequested()) {
 
-			if(gameState != lastGameState) {
-				initState(gameState);
-				lastGameState = gameState;
-			}
+			try {
 
-			int dWheel = Mouse.getDWheel();
-			player.onScrollWheel(dWheel);
-
-			scheduler.update();
-			camera.movement();
-
-			for(MPlayer mplayer : multiplayer.getPlayers()) {
-
-				if(mplayer.entity == null) {
-					mplayer.entity = new EntityTree(loader, mplayer.x, mplayer.z, terrain1, 10);
-					GLog.info("Added Entity to mplayer.entity (" + mplayer.id + "");
+				if(gameState != lastGameState) {
+					initState(gameState);
+					lastGameState = gameState;
 				}
-				mplayer.entity.setPosition(mplayer.x, mplayer.y, mplayer.z);
-				//mplayer.entity.setRotation(mplayer.rx, mplayer.ry, mplayer.rz);
-			}
 
-			if(gameState == GameState.PLAYING && player != null && multiplayer.isConnected()) {
-				sendPlayerPosition();
-			}
+				int dWheel = Mouse.getDWheel();
+				player.onScrollWheel(dWheel);
+
+				scheduler.update();
+				camera.movement();
+
+				for(MPlayer mplayer : multiplayer.getPlayers()) {
+
+					if(mplayer.entity == null) {
+						mplayer.entity = new EntityTree(loader, mplayer.x, mplayer.z, terrain1, 10);
+						GLog.info("Added Entity to mplayer.entity (" + mplayer.id + "");
+					}
+					mplayer.entity.setPosition(mplayer.x, mplayer.y, mplayer.z);
+					//mplayer.entity.setRotation(mplayer.rx, mplayer.ry, mplayer.rz);
+				}
+
+				if(gameState == GameState.PLAYING && player != null && multiplayer.isConnected()) {
+					sendPlayerPosition();
+				}
 
 
-			ParticleMaster.update(camera);
+				ParticleMaster.update(camera);
 
-			for(Terrain terrain : terrains) {
-				for (int i = 0; i < entities.size(); ++i) {
-					Entity entity = entities.get(i);
-					if (entity instanceof EntityMoveable) {
-						if(terrain.getX() <= entity.getPosition().x) { 
-							if(terrain.getX() + terrain.getSize() > entity.getPosition().x) {
-								if(terrain.getZ() <= entity.getPosition().z) {
-									if(terrain.getZ() + terrain.getSize() > entity.getPosition().z) {
-										((EntityMoveable)entity).move(terrain);
+				for(Terrain terrain : terrains) {
+					for (int i = 0; i < entities.size(); ++i) {
+						Entity entity = entities.get(i);
+						if (entity instanceof EntityMoveable) {
+							if(terrain.getX() <= entity.getPosition().x) { 
+								if(terrain.getX() + terrain.getSize() > entity.getPosition().x) {
+									if(terrain.getZ() <= entity.getPosition().z) {
+										if(terrain.getZ() + terrain.getSize() > entity.getPosition().z) {
+											((EntityMoveable)entity).move(terrain);
+										}
 									}
 								}
 							}
 						}
 					}
 				}
-			}
 
 
 
 
 
-			AudioMaster.setListenerData(camera);
+				AudioMaster.setListenerData(camera);
 
-			for(Source source:AudioMaster.sources) {
-				source.tick();
-			}
+				for(Source source:AudioMaster.sources) {
+					source.tick();
+				}
 
-			//rainParticles.generateParticles(player.getPosition().x, player.getPosition().z);
+				//rainParticles.generateParticles(player.getPosition().x, player.getPosition().z);
 
-			//Game renderer
-			GL11.glEnable(GL30.GL_CLIP_DISTANCE0);
+				//Game renderer
+				GL11.glEnable(GL30.GL_CLIP_DISTANCE0);
 
-			//TODO: Animated textures are 3x faster
-			//Render the scene once and store it in the water frame buffer - reflection
-			fbos.bindReflectionFrameBuffer();
-			float distance = 2 * (camera.getPosition().y - waterTile.getHeight());
-			camera.getPosition().y -= distance;
-			camera.invertPitch();
-			renderer.renderScene(lights, camera, terrains, entities, multiplayer.getPlayers(), new Vector4f(0, 1, 0, -waterTile.getHeight()));
-			camera.getPosition().y += distance;
-			camera.invertPitch();
-			
-			//Render the scene once and store it in the water frame buffer - refraction
-			fbos.bindRefractionFrameBuffer();
-			renderer.renderScene(lights, camera, terrains, entities, multiplayer.getPlayers(), new Vector4f(0, -1, 0, waterTile.getHeight()));
-			
-			//Actually render the game to the screen
-			GL11.glDisable(GL30.GL_CLIP_DISTANCE0);
-			fbos.unbindCurrentFrameBuffer();
-			renderer.renderScene(lights, camera, terrains, entities, multiplayer.getPlayers(), new Vector4f(0, 1, 0, 1000000)); //1000000 so nothing will be clipped, hacky workaround because some drivers ignore GL11.glDisable(GL30.GL_CLIP_DISTANCE0); 
-			
-			renderer.renderWater(water, camera);
+				//TODO: Animated textures are 3x faster
+				//Render the scene once and store it in the water frame buffer - reflection
+				fbos.bindReflectionFrameBuffer();
+				float distance = 2 * (camera.getPosition().y - waterTile.getHeight());
+				camera.getPosition().y -= distance;
+				camera.invertPitch();
+				renderer.renderScene(lights, camera, terrains, entities, multiplayer.getPlayers(), new Vector4f(0, 1, 0, -waterTile.getHeight()));
+				camera.getPosition().y += distance;
+				camera.invertPitch();
 
+				//Render the scene once and store it in the water frame buffer - refraction
+				fbos.bindRefractionFrameBuffer();
+				renderer.renderScene(lights, camera, terrains, entities, multiplayer.getPlayers(), new Vector4f(0, -1, 0, waterTile.getHeight()));
 
-			//render last
+				//Actually render the game to the screen
+				GL11.glDisable(GL30.GL_CLIP_DISTANCE0);
+				fbos.unbindCurrentFrameBuffer();
+				renderer.renderScene(lights, camera, terrains, entities, multiplayer.getPlayers(), new Vector4f(0, 1, 0, 1000000)); //1000000 so nothing will be clipped, hacky workaround because some drivers ignore GL11.glDisable(GL30.GL_CLIP_DISTANCE0); 
+
+				renderer.renderWater(water, camera);
 
 
-			ParticleMaster.renderParticles(camera);
+				//render last
 
-			guiRenderer.render(guis);
-			for(Gui gui:guis) {
-				if(gui.isVisible()) { //Need to find a way to find all overlayed guis, and if the z index is over, then remove the text
-					for(GuiText text:gui.getTextsToBeRendered()) {
-						TextMaster.loadText(text);
-					}
-					TextMaster.render();
-					for(GuiText text:gui.getTextsToBeRendered()) {
-						TextMaster.removeText(text);
+
+				ParticleMaster.renderParticles(camera);
+
+				guiRenderer.render(guis);
+				for(Gui gui:guis) {
+					if(gui.isVisible()) { //Need to find a way to find all overlayed guis, and if the z index is over, then remove the text
+						for(GuiText text:gui.getTextsToBeRendered()) {
+							TextMaster.loadText(text);
+						}
+						TextMaster.render();
+						for(GuiText text:gui.getTextsToBeRendered()) {
+							TextMaster.removeText(text);
+						}
 					}
 				}
-			}
-			
-			if(BetterKeyboard.wasKeyPressed(Keyboard.KEY_F5)) {
-				wireframe = !wireframe;
-				GLog.info("Wireframe: + wireframe");
-				if(wireframe) {
-					GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
-				       GL11.glDisable(GL11.GL_TEXTURE_2D);
-				} else {
-					GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
-					GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+				if(BetterKeyboard.wasKeyPressed(Keyboard.KEY_F5)) {
+					wireframe = !wireframe;
+					GLog.info("Wireframe: + wireframe");
+					if(wireframe) {
+						GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+						GL11.glDisable(GL11.GL_TEXTURE_2D);
+					} else {
+						GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
+						GL11.glEnable(GL11.GL_TEXTURE_2D);
+					}
 				}
+
+				//Random keyboard stuff
+				if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+					exit();
+				}
+
+				DisplayManager.updateDisplay();
 			}
-			
-			//Random keyboard stuff
-			if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
-				exit();
+			catch(Exception e) {
+				e.printStackTrace();
 			}
 
-			DisplayManager.updateDisplay();
 		}
 
 		//*****Clean up*********
@@ -422,24 +430,24 @@ public class Main {
 		GLog.info("Exiting Game");
 		ParticleMaster.cleanUp();
 		GLog.info("Cleanup: ParticleMaster");
-		
+
 		AudioMaster.cleanUp();
 		GLog.info("Cleanup: AudioMaster");
-		
+
 		TextMaster.cleanUp();
 		GLog.info("Cleanup: TextMaster");
-		
+
 		guiRenderer.cleanUp();
 		GLog.info("Cleanup: guiRenderer");
-		
+
 		renderer.cleanUp();
 		GLog.info("Cleanup: renderer");
-		
+
 		loader.cleanUp();
 		GLog.info("Cleanup: loader");
 
 		DisplayManager.closeDisplay();
-		
+
 	}
 
 	//Called when ever you set the state
@@ -478,7 +486,7 @@ public class Main {
 
 		}
 	}
-	
-	
+
+
 
 }
